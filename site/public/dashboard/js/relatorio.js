828274199aea6c2b0f69babd8f2b3439bfbf5d15
NathanFrancesco03 <nathan.sepulvida@sptech.school>
2023-05-22 12:43:34 -0300

    Validando os campos da página relatórios

--- site/public/dashboard/js/relatorio.js
+++ site/public/dashboard/js/relatorio.js
@@ -1,16 +1,46 @@
-<script src="pdfmake.min.js"></script>
+var radioButtons = document.getElementsByName('bolinha');
+var tituloInput = document.getElementById('titulo');
+var descricaoInput = document.getElementById('descricao');
+var responsavelInput = document.getElementById('responsavel');
 
-function gerarPdf(){
-    var docDefinition = {
-        content: [
-          'Olá, mundo!' // Conteúdo do documento, pode ser qualquer coisa suportada pela biblioteca
-        ]
-      };
-      
-      // Crie o documento PDF
-      var pdfDoc = pdfMake.createPdf(docDefinition);
+function validarCampo() {
+    var opcaoSelecionada = false;
 
-      pdfDoc.download('arquivo.pdf');
+    for (var i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            opcaoSelecionada = true;
+            break;
+        }
+    }
 
-      pdfDoc.open();
+    if(!tituloInput.value || !descricaoInput.value || !responsavelInput.value ){
+        alert("Preencha os campo corretamente");
+        return false
+    }else if (!opcaoSelecionada) {
+        alert("Selecione uma opção!");
+        return false; 
+    }
+
+    return true;
 }
+
+const bntGenerate = document.querySelector("#generate-pdf");
+
+bntGenerate.addEventListener("click", () =>{
+    
+
+    if(validarCampo() == true){
+            const content = document.querySelector(".relatorio-conteudo")
+
+            var opt = {
+            margin:       1,
+            filename:     'ErrorEagle-relatório.pdf',
+            image:        { type: 'jpeg', quality: 0.98 },
+            html2canvas:  { scale: 2 },
+            jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+            };
+
+            html2pdf().set(opt).from(content).save();
+    }
+
+})
