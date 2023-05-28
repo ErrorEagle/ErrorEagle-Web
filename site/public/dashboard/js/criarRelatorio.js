aa371548b3002d8af0bc359bfb8ed942bf81a1b0
fernandeess <matheus.rodrigues@sptech.school>
2023-05-28 17:46:03 -0300

    fix: funcionando na nova pagina

--- site/public/dashboard/js/criarRelatorio.js
+++ site/public/dashboard/js/criarRelatorio.js
@@ -1,7 +1,7 @@
-var tituloInput = document.getElementById('titulo');
-var descricaoManutencaoInput = document.getElementById('descricaoManutencao');
-var descricaoIncidenteInput = document.getElementById('descricaoIncidente');
-var dataManutencaoInput = document.getElementById('dataM')
+
+
+
+
 
 var dataAtual = new Date();
 var dia = dataAtual.getDate();
@@ -9,34 +9,49 @@ var mes = dataAtual.getMonth() + 1;
 var ano = dataAtual.getFullYear();
 if (dia < 10) {
     dia = '0' + dia;
-  }
-  
-  if (mes < 10) {
+}
+
+if (mes < 10) {
     mes = '0' + mes;
-  }
-  
+}
+
 var dataFormatada = ano + "-" + mes + "-" + dia;
 
 console.log(dataFormatada);
+
+
 
 function validarAno(dataManutencaoInput) {
     var data = new Date(dataManutencaoInput);
     var ano = data.getFullYear();
     var anoAtual = dataAtual.getFullYear();
-  
+
     if (isNaN(data.getTime())) {
-      return false;
+        return false;
     } else if (ano == anoAtual) {
-      return true;
+        return true;
     } else {
-      return false;
+        return false;
     }
-  }
+}
 
 function gerarRelatorio() {
+    var tituloInput = document.getElementById('titulo');
+    var descricaoManutencaoInput = document.getElementById('descricaoManutencao');
+    var descricaoIncidenteInput = document.getElementById('descricaoIncidente');
+    var dataManutencaoInput = document.getElementById('dataM')
     var fkFuncionario = sessionStorage.ID_FUNCIONARIO
     var fkEmpresa = sessionStorage.FK_EMPRESA
-    var fkMaquina = '1'
+    var fkMaquina = sessionStorage.getItem("ID_MAQUINA");
+    console.log("titulo: " + tituloInput.value)
+    console.log("fkFuncionario " + fkFuncionario.value + "fkEmpresa " + fkEmpresa.value + "fkMaquina " + fkMaquina.value)
+
+    function limparCampos() {
+        tituloInput.value = '';
+        descricaoManutencaoInput.value = '';
+        descricaoIncidenteInput.value = '';
+        dataManutencaoInput.value = '';
+    }
 
     if (!tituloInput.value || !descricaoIncidenteInput.value || !descricaoManutencaoInput.value || !dataManutencaoInput.value || !validarAno(dataManutencaoInput.value)) {
         const Toast = Swal.mixin({
@@ -59,58 +74,54 @@ function gerarRelatorio() {
 
         }, 1000)
     } else {
-    console.log(dataManutencaoInput.value)
-    fetch("/relatorio/gerarRelatorio", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            tituloServer: tituloInput.value,
-            descricaoIncidenteServer: descricaoIncidenteInput.value,
-            descricaoManutencaoServer:descricaoManutencaoInput.value,
-            dataManutencaoServer:dataManutencaoInput.value,
-            dataRelatorioServer:dataFormatada,
-            fkMaquinaServer:"2",
-            fkFuncionarioServer: fkFuncionario,
-            fkEmpresaServer: fkEmpresa
+        console.log(dataManutencaoInput.value)
+        fetch("/relatorio/gerarRelatorio", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                tituloServer: tituloInput.value,
+                descricaoIncidenteServer: descricaoIncidenteInput.value,
+                descricaoManutencaoServer: descricaoManutencaoInput.value,
+                dataManutencaoServer: dataManutencaoInput.value,
+                dataRelatorioServer: dataFormatada,
+                fkMaquinaServer: "2",
+                fkFuncionarioServer: fkFuncionario,
+                fkEmpresaServer: fkEmpresa
+            })
+        }).then(function (resposta) {
+            if (resposta.ok) {
+
+                console.log('Enviado para o banco')
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.addEventListener('mouseenter', Swal.stopTimer)
+                        toast.addEventListener('mouseleave', Swal.resumeTimer)
+                    }
+
+                })
+
+                Toast.fire({
+                    icon: 'success',
+                    title: 'Relatório enviado com sucesso!'
+                })
+                setTimeout(() => {
+
+                }, 20000)
+                limparCampos();
+            } else {
+                console.log('Erro ao enviar para o banco')
+            }
+        }).catch(function (erro) {
+            console.log(erro);
         })
-    }).then(function (resposta) {
-        if(resposta.ok){
-            console.log('Enviado para o banco')
-            const Toast = Swal.mixin({
-                toast: true,
-                position: 'top-end',
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                    toast.addEventListener('mouseenter', Swal.stopTimer)
-                    toast.addEventListener('mouseleave', Swal.resumeTimer)
-                }
-            })
+    }
 
-            Toast.fire({
-                icon: 'success',
-                title: 'Relatório enviado com sucesso!'
-            })
-            setTimeout(() => {
-
-            }, 20000)
-            limparCampos();
-        }else{
-            console.log('Erro ao enviar para o banco')
-        }
-    }).catch(function (erro) {
-        console.log(erro);
-    })
-}
-    
 }
 
-function limparCampos() {
-    tituloInput.value = '';
-    descricaoManutencaoInput.value = '';
-    descricaoIncidenteInput.value = '';
-    dataManutencaoInput.value = '';
-}
