a3f3ff97ae309e9c8f7032e66e338a0f2f31fb82
fernandeess <matheus.rodrigues@sptech.school>
2023-05-29 17:42:32 -0300

    refactor: removi comentarios

--- site/src/controllers/gerenciadorUsuarioController.js
+++ site/src/controllers/gerenciadorUsuarioController.js
@@ -2,47 +2,6 @@ var gerenciadorUsuarioModel = require("../models/gerenciadorUsuarioModel");
 
 var sessoes = [];
 
-// function cadastrarFuncionario(req, res) {
-//     var nomeFuncionario = req.body.nomeFuncionarioServer;
-//     var emailFuncionario = req.body.emailFuncionarioServer;
-//     var senhaFuncionario = req.body.senhaFuncionarioServer;
-//     var telefoneFuncionario = req.body.telefoneFuncionarioServer;
-//     var empresaFuncionario = req.body.empresaFuncionarioServer;
-//     var fkSupervisor = req.body.fkSupervisorServer;
-
-
-//     if (nomeFuncionario == undefined) {
-//         res.status(400).send("Seu nome está undefined!");
-//     } else if (emailFuncionario == null) {
-//         res.status(400).send("Seu email está undefined!");
-//     } else if (senhaFuncionario == null) {
-//         res.status(400).send("Sua senha está undefined!");
-//     } else if (telefoneFuncionario == null) {
-//         res.status(400).send("Seu telefoneFuncionario está undefined!");
-//     } else if (empresaFuncionario == null) {
-//         res.status(400).send("Sua empresa está undefined!");
-//     } else if (fkSupervisor == null) {
-//         res.status(400).send("ID resposável está undefined!");
-//     }
-//     else {
-//         gerenciadorUsuarioModel.cadastrarFuncionario(nomeFuncionario, emailFuncionario, senhaFuncionario, telefoneFuncionario, empresaFuncionario, fkSupervisor).then(
-//             function (resultado) {
-//                 res.json(resultado);
-//             }
-//         ).catch(
-//             function (erro) {
-//                 console.log(erro);
-//                 console.log(
-//                     "\nHouve um erro ao realizar o cadastro! Erro: ",
-//                     erro.sqlMessage
-//                 );
-//                 res.status(500).json(erro.sqlMessage);
-//             }
-//         )
-//     }
-
-
-// }
 
 function deletarUsuario(req, res) {
     var idFuncionario = req.params.funcionario;
@@ -84,14 +43,10 @@ function listarFuncionarios(req, res) {
                     console.log(`\nResultados encontrados: ${resultado.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultado)}`); // transforma JSON em String
 
-                    // if (resultado.length == 1) {
+
                     console.log(resultado);
                     res.json(resultado);
-                    // } else if (resultado.length == 0) {
-                    //     res.status(403).send("Email e/ou senha inválido(s)");
-                    // } else {
-                    //     res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-                    // }
+
                 }
             ).catch(
                 function (erro) {
@@ -120,14 +75,10 @@ function listarFuncionarios(req, res) {
                     console.log(`\nResultados encontrados: ${resultado.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultado)}`); // transforma JSON em String
 
-                    // if (resultado.length == 1) {
+
                     console.log(resultado);
                     res.json(resultado);
-                    // } else if (resultado.length == 0) {
-                    //     res.status(403).send("Email e/ou senha inválido(s)");
-                    // } else {
-                    //     res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-                    // }
+
                 }
             ).catch(
                 function (erro) {
@@ -156,14 +107,9 @@ function listarFuncionariosInativos(req, res) {
                     console.log(`\nResultados encontrados: ${resultado.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultado)}`); // transforma JSON em String
 
-                    // if (resultado.length == 1) {
                     console.log(resultado);
                     res.json(resultado);
-                    // } else if (resultado.length == 0) {
-                    //     res.status(403).send("Email e/ou senha inválido(s)");
-                    // } else {
-                    //     res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-                    // }
+
                 }
             ).catch(
                 function (erro) {
@@ -194,14 +140,10 @@ function listarFuncionariosInativos(req, res) {
                     console.log(`\nResultados encontrados: ${resultado.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultado)}`); // transforma JSON em String
 
-                    // if (resultado.length == 1) {
+
                     console.log(resultado);
                     res.json(resultado);
-                    // } else if (resultado.length == 0) {
-                    //     res.status(403).send("Email e/ou senha inválido(s)");
-                    // } else {
-                    //     res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-                    // }
+
                 }
             ).catch(
                 function (erro) {
