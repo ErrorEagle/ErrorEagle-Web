2c4d304aa6698121f03c1a2db9007441a1a2a1de
GuilhermeCocoloti <guilherme.moraes@sptech.school>
2023-06-01 17:47:59 -0300

    Alterações front atualizarSenha

--- site/public/dashboard/js/sidebar.js
+++ site/public/dashboard/js/sidebar.js
@@ -1,17 +1,12 @@
-// const sidebar = document.querySelector('.sidebar');
 
-// // Define um temporizador que aguarda 20 segundos e, em seguida, fecha a barra lateral.
-// setTimeout(() => {
-//     sidebar.style.width = '30';
-// }, 2000);
+//SIDEBAR
 
-<<<<<<< HEAD
 nomeSideBar.innerHTML += " " + sessionStorage.getItem("NOME_USUARIO");
 // empresaSideBar.innerHTML += " " + sessionStorage.FK_EMPRESA;
+
+//TITULO ATUALIZAR SENHA
 var nomeDiv = document.getElementById("nomeTitulo")
 nomeDiv.innerHTML += `Olá, ${sessionStorage.getItem("NOME_USUARIO")}!`
-console.log("inserirNome");
-=======
-nomeSideBar.innerHTML += " " + sessionStorage.NOME_USUARIO;
-empresaSideBar.innerHTML += " " + sessionStorage.NOME_EMPRESA;
->>>>>>> e7bbe61b6fa9a1afff6badb81229ecacef1d866e
+
+
+
